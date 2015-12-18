0da9dff6bae676e4edfb5084455b6be1f9b8362f
Max Cantor <max@maxcantor.net>
2015-12-18 15:59:39 -0500

    Completely stub out the controller.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,17 +1,10 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
   end
 
   def create
-    @author = Author.create(
-      name: params[:name],
-      email: params[:email]
-    )
-
-    redirect_to author_path(@author)
   end
 end
